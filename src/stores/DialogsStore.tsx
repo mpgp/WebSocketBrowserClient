56f0aa0ba3a770a6672022576ff8beccb08aee96
loktionov129 <loktionov129@gmail.com>
2018-02-17 11:02:06 +0700

    [dialogs]: fix DialogsStore error with 0 items

--- src/stores/DialogsStore.tsx
+++ src/stores/DialogsStore.tsx
@@ -23,24 +23,10 @@ export interface Dialog {
     Items: DialogMessage[];
 }
 
-const dialogsList: Dialog[] = [{
-    Login: 'test',
-    Items: [
-        { Login: 'test', Message: 'Hello', Time: 1897414 },
-        { Login: 'admin2018', Message: 'Welcome to our chat!', Time: 1897424 },
-    ]
-}, {
-    Login: 'Scorpio92',
-    Items: [
-        { Login: 'Scorpio92', Message: 'RSA + AES ?', Time: 277435 },
-        { Login: 'admin2018', Message: 'HTTPS + WSS !', Time: 277469 },
-    ]
-}];
-
 class DialogsStore {
     private static readonly defaultTitle = <span>Dialog list</span>;
 
-    @observable dialogs: Dialog[] = dialogsList;
+    @observable dialogs: Dialog[] = [];
     @observable title = DialogsStore.defaultTitle;
     @observable mode = DialogsStoreMode.DIALOGS_LIST;
     @observable isOpen = false;
@@ -111,6 +97,7 @@ class DialogsStore {
     }
 
     private onTitleClick() {
+        this.dialogs = this.dialogs.filter((dialog) => dialog.Items.length > 0);
         this.resetTitle();
         this.setMode(DialogsStoreMode.DIALOGS_LIST);
     }
